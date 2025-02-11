ce8403918f612cdd39a01167b1d7c7a5c08c6c60
selva satheesh <selvasathish@kenshinsystems.com>
2025-02-12 00:17:30 +0530

    Feat : Add Spring Security 6

---
+++ src/main/java/com/example/blog/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.blog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/")
+                        .permitAll()
+                )
+                .build();
+    }
+}
---
+++ src/main/java/com/example/blog/models/Users.java
@@ -0,0 +1,13 @@
+package com.example.blog.models;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import jakarta.persistence.Entity;
+
+
+@Entity
+public class Users implements User{
+    
+}
